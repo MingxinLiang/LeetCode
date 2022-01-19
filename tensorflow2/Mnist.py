9fa9688970d42efb73f2cb7d4f089291539db48f
Soldate <soldate@live.com>
2022-01-19 22:42:25 +0800

    ADD: DataLoad

--- tensorflow2/Mnist.py
+++ tensorflow2/Mnist.py
@@ -24,22 +24,22 @@ def train_step(images, labels):
         # training=True is only needed if there are layers with different behavior during training versus inference (e.g. Dropout).
         predictions = model(images, training = True)
         loss = loss_object(labels, predictions)
-    
+
     gradient = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradient, model.trainable_variables))
-    
+
     train_loss(loss)
     train_accuracy(labels, predictions)
-    
-    
+
+
 @tf.function
 def test_step(images, labels):
     predictions = model(images, training = False)
     t_loss = loss_object(labels, predictions)
-    
+
     test_loss(t_loss)
     test_accuracy(labels, predictions)
-    
+
 if __name__ == "__main__":
     EPOCHS = 5
 
@@ -66,30 +66,29 @@ if __name__ == "__main__":
 
     optimizer = tf.keras.optimizers.Adam()
 
-    train_loss = tf.keras.metrics.Mean(name = "train_loss") 
+    train_loss = tf.keras.metrics.Mean(name = "train_loss")
     train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name = "train_accuracy")
 
     test_loss = tf.keras.metrics.Mean(name = "test_loss")
     test_accuracy = tf.keras.metrics.SparseCategoricalCrossentropy(name = "test_accuracy")
 
+    for epoch in range(EPOCHS):
+        # Reset the metrics at the start of the next epoch
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        test_loss.reset_states()
+        test_accuracy.reset_states()
 
-for epoch in range(EPOCHS):
-    # Reset the metrics at the start of the next epoch
-    train_loss.reset_states()
-    train_accuracy.reset_states()
-    test_loss.reset_states()
-    test_accuracy.reset_states()
+        for images, labels in train_ds:
+            train_step(images, labels)
 
-    for images, labels in train_ds:
-        train_step(images, labels)
+        for test_images, test_labels in test_ds:
+            test_step(test_images, test_labels)
 
-    for test_images, test_labels in test_ds:
-        test_step(test_images, test_labels)
-
-    print(
-        f'Epoch {epoch + 1}, '
-        f'Loss: {train_loss.result()}, '
-        f'Accuracy: {train_accuracy.result() * 100}, '
-        f'Test Loss: {test_loss.result()}, '
-        f'Test Accuracy: {test_accuracy.result() * 100}'
-    )
+        print(
+            f'Epoch {epoch + 1}, '
+            f'Loss: {train_loss.result()}, '
+            f'Accuracy: {train_accuracy.result() * 100}, '
+            f'Test Loss: {test_loss.result()}, '
+            f'Test Accuracy: {test_accuracy.result() * 100}'
+        )
