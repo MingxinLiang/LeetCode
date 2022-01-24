bdaa98bc0fff369add7dbb067f643d27e07dfc71
Soldate <soldate@live.com>
2022-01-24 23:31:52 +0800

    mend

---
+++ tensorflow2/ASR/TransLearnForASR.py
@@ -0,0 +1,22 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+import tensorflow_hub as hub
+#import tensorflow_io as tfio
+
+import IPython as dispaly
+import matplotlib.pyplot as plt
+
+#yamnet_model_handle = 'https://tfhub.dev/google/yamnet/1'
+#yamnet_model = hub.load(yamnet_model_handle)
+
+#testing_wav_file_name = tf.keras.utils.get_file('miaow_16k.wav',
+#                                                'https://storage.googleapis.com/audioset/miaow_16k.wav',
+#                                                cache_dir='./',
+#                                                cache_subdir='test_data')
+
+#print(testing_wav_file_name)
+
